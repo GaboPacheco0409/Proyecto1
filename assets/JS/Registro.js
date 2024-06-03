5f5200b6997629d78a6ceb2c22ba4c964fe4aa4e
GaboPacheco0409 <164896794+GaboPacheco0409@users.noreply.github.com>
2024-06-03 12:24:38 -0400

    Add files via upload

--- assets/JS/Registro.js
+++ assets/JS/Registro.js
@@ -1,29 +1,11 @@
-function registro(){
-A = document.getElementById("Usuario")
+document.getElementById("formulario1").addEventListener("submit", function(event) {
+    event.preventDefault();
+    let correo = document.querySelector(".input-field[type='email']").value;
+    let contraseña = document.querySelector(".input-field[type='password']").value;
 
+    localStorage.setItem("correo", correo);
+    localStorage.setItem("pwd1", contraseña);
 
-
-
-
-B = document.getElementById("correo")
-
-    
-
-C = document.getElementById("pwd")
-return localStorage.setItem("a1", A),
-localStorage.setItem("b1", B),
-localStorage.setItem("c1", C)
-
-}
-function inicio(){
-var datoGuardado1 = localStorage.getItem("a1")
-var datoGuardado2 = localStorage.getItem("b1")
-var datoGuardado3 = localStorage.getItem("c1")
-if (datoGuardado1 === document.getElementById("Usuario1").value || datoGuardado2 === document.getElementById("Usuario1").value) {
-    if (datoGuardado2 == datoGuardado2){
-            console.log("INICIASTE SESION CON EXITO")
-    }
-    } else {
-        console.log("INICIASTE SESION CON EXITO")
-    }
-}
+    document.getElementById('res1').innerHTML = "Cuenta creada con éxito para " + correo;
+    console.log("Cuenta creada con éxito para " + correo)
+});
