ba930726d09af073c974205df1463e30926fa1b7
GaboPacheco0409 <164896794+GaboPacheco0409@users.noreply.github.com>
2024-06-03 20:04:40 -0400

    Update Inicio_sesion.js

--- assets/JS/Inicio_sesion.js
+++ assets/JS/Inicio_sesion.js
@@ -7,7 +7,7 @@ function validar(){
 
     if (correo == correoCorrecto && contraseña == contraseñaCorrecta) {
         document.getElementById('res1').innerHTML = "Inicio de sesión correcto";
-        window.location.href = 'inicio.html';
+        window.location.href = 'Inicio.html';
         console.log("entro");
     } else {
         document.getElementById('res1').innerHTML = "Correo electrónico o contraseña incorrecta";
